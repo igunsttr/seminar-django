a8fdd49ef69ad00d97d972c0c7a58dbc314dffd9
igunsttr <102118453+igunsttr@users.noreply.github.com>
2025-04-22 13:53:13 +0700

    Add files via upload

--- namaproyek/urls.py
+++ namaproyek/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include
+
 from . import dt_jadwal
 from django.contrib import admin
 from django.urls import path
 from namaproyek import views as v
+
 urlpatterns = [
+    path('grappelli/', include('grappelli.urls')), # grappelli URLS
     path('admin/', admin.site.urls),
     path('',v.index, name='index'),
     path('about_us',v.about_us, name='about_us'),

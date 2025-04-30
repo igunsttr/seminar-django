cbda51e679d6e9cec236f8d9a6263c91acd671c8
igunsttr <102118453+igunsttr@users.noreply.github.com>
2025-04-30 13:14:19 +0700

    Add files via upload

--- namaproyek/views.py
+++ namaproyek/views.py
@@ -1,6 +1,9 @@
 
 from .models import Mahasiswa, Jurusan, Jadwal
 from django.shortcuts import render
+from django import forms
+from .form import Bukutamuform
+from django.shortcuts import render, redirect
 
 def index(request):
 	data=Mahasiswa.objects.all()   # query="select * from mahasiswa"
@@ -11,8 +14,19 @@ def about_us(request):
 	return render(request, 'aboutus.html', context={'dataku':data})
 
 def portovolio(request):
-	data=Mahasiswa.objects.all()
+	data=Mahasiswa.objects.all() 
 	return render(request, 'portovolio.html', context={'dataku':data})
-
 def contact(request):
 	return render(request, 'contact.html')
+
+
+def buku_tamu(request):
+	if request.method == 'POST':
+		form = Bukutamuform(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('/')  #jika berhasil disave, redirect ke index
+	else:
+		form1 = Bukutamuform()
+	return render(request, 'bukutamu.html', {'form': form1})
+
